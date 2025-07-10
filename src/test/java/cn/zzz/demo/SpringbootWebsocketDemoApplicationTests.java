946653469637f013ee33bedc31b72b33daa819d7
zhangzongzheng <751781196@qq.com>
2025-07-10 10:16:05 +0800

    feat: 创建 Spring Boot Websocket演示项目
    - 新增 .gitignore 文件，忽略IDE和构建工具生成的临时文件
    - 添加 application.properties 文件，配置 Spring Boot 应用名称
    - 创建 pom.xml 文件，定义项目依赖和构建配置
    - 编写 README.md，提供项目参考文档和指南- 新增 Spring Boot 主应用类 SpringbootWebsocketDemoApplication
    - 添加单元测试类 SpringbootWebsocketDemoApplicationTests

---
+++ src/test/java/cn/zzz/demo/SpringbootWebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zzz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsocketDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
